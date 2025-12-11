3313bea9632a83fcd3c9ef2a00148f736b261bd0
Schneiderpi <rsnyder3.14159@gmail.com>
2025-12-11 15:30:48 -0500

    Added valid output columns and added option to include image urls in output

--- src/pull_info.py
+++ src/pull_info.py
@@ -3,10 +3,8 @@ import requests
 import tqdm
 import csv
 import enum
+from valid_output_columns import valid_output_columns
 
-valid_output_columns = [
-
-]
 
 courtesy_wait = 100 #Time in ms to wait between requests, Scryfall requests between 50-100ms_
 
@@ -16,6 +14,7 @@ courtesy_wait = 100 #Time in ms to wait between requests, Scryfall requests betw
 @click.option("-i", "--input",type=click.Path(exists=True,dir_okay=False),help="Input filename for a list of cards to return information for, each card should be on its own line")
 @click.option("-c", "--columns",type=click.Choice(valid_output_columns),multiple=True,help="Output column information to include, can be specified multiple times. See https://scryfall.com/docs/api/cards Default is everything but images")
 @click.option("--max",type=int,help="Max cards to pull",default=-1)
+@click.option("--image",type=bool,help="Whether to include image information in output. For now this is in the format =IMAGE(url) for use with Google Sheets")
 def pull():
     """
     Given a list of card names or other search parameters, pulls specified information from Scryfall.
