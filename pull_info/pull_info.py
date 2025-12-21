b23c05c1835e43d2b95083e52a840af970fc983f
Schneiderpi <rsnyder3.14159@gmail.com>
2025-12-21 11:33:08 -0500

    Initial code for pulling card info from names from input file

--- pull_info/pull_info.py
+++ pull_info/pull_info.py
@@ -2,8 +2,15 @@ import click
 import requests
 import tqdm
 import csv
-from pull_info.card import Card
 
+from requests.compat import urljoin
+from time import sleep
+
+from card import Card
+
+
+scryfall_url = "https://api.scryfall.com"
+common_headers = {"User-Agent": "ScryfallPull/.1", "accept": "application/json"}
 
 courtesy_wait = 100 #Time in ms to wait between requests, Scryfall requests between 50-100ms_
 
@@ -14,8 +21,35 @@ courtesy_wait = 100 #Time in ms to wait between requests, Scryfall requests betw
 @click.option("-c", "--columns",type=click.Choice(Card.valid_output_columns),multiple=True,help="Output column information to include, can be specified multiple times. See https://scryfall.com/docs/api/cards Default is everything but images")
 @click.option("--max",type=int,help="Max cards to pull",default=-1)
 @click.option("--image",type=bool,help="Whether to include image information in output. For now this is in the format =IMAGE(url) for use with Google Sheets")
-def pull():
+def pull(out,search,input,columns,max,image):
     """
     Given a list of card names or other search parameters, pulls specified information from Scryfall.
     """
-    pass
+    card_names = []
+
+    if input is not None:
+        with open(input) as f:
+            card_names = [line.strip() for line in f]
+    
+
+    cards = []
+    if len(card_names) > 0:
+        for card in card_names:
+            cards.append(Card(pull_card(card,columns)))
+            sleep(courtesy_wait/1000)
+
+def pull_card(name):
+    uri = "/cards/named"
+    options = {"fuzzy": name}
+    headers = common_headers
+
+    url = urljoin(scryfall_url,uri)
+
+    response = requests.get(url,params=options,headers=headers)
+
+    response.raise_for_status()
+
+    return response.json()
+
+if __name__ == '__main__':
+    pull()
