3b481ff64f96444888c6905aea777e51e628f85e
Schneiderpi <rsnyder3.14159@gmail.com>
2026-01-31 19:56:00 -0500

    removed todo on output command

--- pull_info/pull_info.py
+++ pull_info/pull_info.py
@@ -17,7 +17,7 @@ common_headers = {"User-Agent": "ScryfallPull/.1", "accept": "application/json"}
 courtesy_wait = 100 #Time in ms to wait between requests, Scryfall requests between 50-100ms_
 
 @click.command()
-@click.option("-o", "--out", type=click.Path(dir_okay=False,writable=True),help="Filename to store output. Output will be in csv and file WILL BE OVERWRITTEN") #TODO
+@click.option("-o", "--out", type=click.Path(dir_okay=False,writable=True),help="Filename to store output. Output will be in csv and file WILL BE OVERWRITTEN")
 @click.option("-s", "--search", type=str,multiple=True,help="Parameters to search by, see full documentation for formatting. Can be provided multiple times.") #TODO
 @click.option("-i", "--input",type=click.Path(exists=True,dir_okay=False),help="Input filename for a list of cards to return information for, each card should be on its own line")
 @click.option("-c", "--columns",type=click.Choice(Card.valid_output_columns),multiple=True,help="Output column information to include, can be specified multiple times. See https://scryfall.com/docs/api/cards Default is everything but images, which is handled separately")
